3ebc0e914d3d8b9958b9114c4de8b4cf4ca274fa
Mahammad Naushad <mahammadnaushad07@gmail.com>
2025-03-07 21:48:10 +0530

    Update equal_occurence.py

--- equal_occurence.py
+++ equal_occurence.py
@@ -6,11 +6,9 @@ You are given a string, you need to return True if  the words "cat" and "hat" ap
 def check_for_equal_occurence(string):
     cat_count = 0
     hat_count = 0
-    for i in range(len(string)-2):
-        if string[i] == "c" and string[i:i + 3] == "cat":
-            cat_count += 1
-        elif string[i] == "h" and string[i:i + 3] == "hat":
-            hat_count += 1
+
+    cat_count = string.count("cat")
+    hat_count = string.count("hat")
 
     return cat_count == hat_count
 
